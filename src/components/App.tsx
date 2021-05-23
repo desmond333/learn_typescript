d56e9e9e55056ad6bba7cefd32e58cdd69c47230
Slava <my_post_office1@mail.ru>
2021-05-23 17:48:38 +0300

    Added structure of app

---
+++ src/components/App.tsx
@@ -0,0 +1,7 @@
+import { useState, useEffect, useRef } from 'react';
+
+import { ITodo } from '../types/data';
+
+export const App: React.FunctionComponent = () => {
+  return <div>Hello, world!</div>;
+};
